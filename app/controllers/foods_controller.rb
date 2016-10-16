d8a7faad330c38a227b5d0d75e63d24c4af46893
Touqeer Ahmad <Touqeer Ahmad>
2016-10-16 16:06:43 +0500

    make it better

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,6 +1,6 @@
 class FoodsController < ApplicationController
   def index
-  	@logo="/assets/foodicted_logo.png"
+  	@logo="/images/foodicted_logo.png"
   	@search_term = params[:looking_for] || 'noodle'
   	@foods = Food.for(@search_term)
   end
